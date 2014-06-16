b4a7fe04a1ed45346306a0d5ddf3ec032e9c69ee
Fedya Skitsko <f.skitsko@brightgrove.com>
2014-06-16 13:46:43 +0300

    Avoid divide by zero

--- SwiftReference.playground/section-1.swift
+++ SwiftReference.playground/section-1.swift
@@ -18,6 +18,10 @@ let implicitDouble = 70.0
 let explicitDouble: Double = 70
 let testConst :Float = 4
 
+// Avoid divide by zero, returns 0 when second parameter is zero
+10 &/ 5
+10 &/ 0
+
 // Type Casting
 let label = "The width is "
 let width = 94
